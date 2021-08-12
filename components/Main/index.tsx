6f20731d718ed10a2f48b655a41521812df3f9c9
DESKTOP-K249EP1\NotAUser <wrongnmail@yandex.ru>
2021-08-12 18:41:23 +0300

    STARMOON-2

--- components/Main/index.tsx
+++ components/Main/index.tsx
@@ -10,18 +10,44 @@ export default function Main(props: Props) {
   return (
     <body className={styles.body}>
       <div className={styles.root}>
+        <div className={styles.cloudsRight}>
+          <img src='/img/Main/cloudsRight.svg' alt=''/>
+        </div>
+        <div className={styles.cloudsLeft}>
+          <img src='/img/Main/cloudsLeft.svg' alt=''/>
+        </div>
+        <div className={styles.hills}>
+          <img src='/img/Main/hills.svg' alt=''/>
+        </div>
         <div className={styles.grass}>
           <img src='/img/Main/grass.svg' alt=''/>
         </div>
+          <div className={styles.celestials}>
+            <div className={styles.comet}>
+              <img src='/img/Main/comet.svg' alt=''/>
+            </div>
+            <div className={styles.moon}>
+              <img src='/img/Main/moon.svg' alt=''/>
+            </div>
+          </div>
         <div className={styles.container}>
-          <div className={styles.textBlock}>
-            Приложение которое<br/> улучшит вашу жизнь
-          </div>
-          <div className={styles.stargazer}>
-            <img src='/img/Main/stargazer.svg' alt=''/>
-          </div>
+          <div className={styles.bottom}>
+            <div className={styles.textBlock}>
+              <span>Приложение которое<br/> улучшит вашу жизнь</span>
+              <div className={styles.btns}>
+                <div className={styles.firstBtn}><Button image='/img/Main/google.svg' size='noPadding'/></div>
+                <Button image='/img/Main/apple.svg' size='noPadding'/>
+              </div>
+            </div>
+            <div className={styles.cloud}>
+              <img src='/img/Main/cloud.svg' alt=''/>
+            </div>
+            <div className={styles.stargazer}>
+              <img src='/img/Main/stargazer.svg' alt=''/>
+            </div>
           </div>
         </div>
+      </div>
     </body>
   )
 }
--- components/layout/Header/index.tsx
+++ components/layout/Header/index.tsx
@@ -2,12 +2,16 @@ import Logo from 'components/svg/Logo'
 import styles from './index.module.scss'
 import Link from 'next/link'
 import Button from 'components/ui/Button'
+import DynamicOverflow from 'utils/DynamicOverflow'
+import ButtonDotsWithOverflow from 'components/ui/Button/ButtonDotsWithOverflow';
+import cx from 'classnames'
+import { useEffect, useState } from 'react'
 
 interface Props {}
 
 export default function Header(props: Props) {
 
-  const menu = [{label: 'Гороскоп', link: '#'},
+  const options = [{label: 'Гороскоп', link: '#'},
   {label: 'Аффирмации', link: '#'},
   {label: 'Мудрость', link: '#'},
   {label: 'Исцеление', link: '#'},
@@ -16,24 +20,64 @@ export default function Header(props: Props) {
   {label: 'Шар предсказаний', link: '#'},
   {label: 'Древо жизни', link: '#'},
 ]
-  
+
+const [isScrolled, setIsScrolled] = useState(false)
+
+const handleScroll = () => {
+  if (window.pageYOffset > 0) {
+    setIsScrolled(true)
+  }
+  else{
+    setIsScrolled(false)
+  }
+}
+
+useEffect(() => {
+  window.addEventListener("scroll", handleScroll);
+})
+
+
   return (
-    <div className={styles.root}>
+    <div className={cx(styles.root, {[styles.scrolled]: isScrolled})}>
       <div className={styles.container}>
         <div className={styles.logo}>
           <Logo/>
           <div className={styles.title}>Starmoon</div>
         </div>
-        <div className={styles.menu}>
-          {menu.map(item => 
-            <Link href={item.link}>
-              <a className={styles.item}>{item.label}</a>
-            </Link>
-          )}
-        </div>
+        <DynamicOverflow
+          list={({ nodeRef }) => [
+            ...options.map((item, index) => (
+              <Link href={item.link}>
+                <a
+                  className={styles.item}
+                  href={item.link}
+                  ref={nodeRef}
+                  key={index + 1}
+                >
+                  {item.label}
+                </a>
+              </Link>
+            )),
+          ]}
+        >
+          {({ visibleElements, overflowElements, containerRef, moreNodeRef }) => {
+            return (
+              <div ref={containerRef} className={styles.list}>
+                {visibleElements}
+                {overflowElements.length > 0 && (
+                  <div className={styles.dots} ref={moreNodeRef}>    
+                    <ButtonDotsWithOverflow>
+                        {overflowElements}
+                    </ButtonDotsWithOverflow>
+                  </div>
+                )}
+              </div>
+            )
+          }}
+        </DynamicOverflow>
         <div className={styles.btns}>
           <div className={styles.firstBtn}>
-            <Button variant='outlined' wrapper size='small' fontColor='pink'>вход</Button>
+            <Button className={isScrolled && styles.black} variant='outlined' wrapper size='small' fontColor='pink'>вход</Button>
           </div>
           <Button size='small' color='fill'>регистрация</Button>
         </div>
