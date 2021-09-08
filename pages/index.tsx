6ae83bbfd8489f1f36029585ae388365ad53a9ef
DESKTOP-K249EP1\NotAUser <wrongnmail@yandex.ru>
2021-09-08 13:14:35 +0300

    STARMOON-32

--- pages/index.tsx
+++ pages/index.tsx
@@ -27,49 +27,49 @@ export default function Home(props) {
       </div>
         <div className={styles.content}>
         <Content lottie={<Horoscope/>} image='/img/Content/horoscope.svg' title='ГОРОСКОП НА КАЖДЫЙ ДЕНЬ' 
-        desc='упорядоченное отображение взаимного расположения планет на звёздном небе в определенный промежуток времени по знакам зодиака. Используется в астрологии с целью предсказания судьбы.' 
+        desc='узнай что ждет тебя завтра и спланируй день максимально интересно' 
         color='blue'/>
         <div className={styles.waves2} id='affirmatio'>
           <img src='/img/backgrounds/waves2.svg' alt=''/>
         </div>
         <Content lottie={<Affirmatio/>} image='/img/Content/affirmatio.svg' title='АФФИРМАЦИИ' 
-        desc='упорядоченное отображение взаимного расположения планет на звёздном небе в определенный промежуток времени по знакам зодиака. Используется в астрологии с целью предсказания судьбы.' 
+        desc='создай правильный психологический настрой для успешной жизни' 
         color='black' right/>
         <div className={styles.waves} id='wisdom'>
           <img src='/img/backgrounds/waves2.svg' alt=''/>
         </div>
         <Content lottie={<Wisdom/>} image='/img/Content/wisdom.svg' title='MУДРОСТЬ' 
-        desc='упорядоченное отображение взаимного расположения планет на звёздном небе в определенный промежуток времени по знакам зодиака. Используется в астрологии с целью предсказания судьбы.' 
+        desc='воспользуйся простыми подсказками от опытных гуру, чтобы уже сегодня твоя жизнь стала лучше и продуктивнее' 
         color='blue'/>
         <div className={styles.waves2} id='healing'>
           <img src='/img/backgrounds/waves2.svg' alt=''/>
         </div>
         <Content lottie={<Healing/>} image='/img/Content/healing.svg' title='ИСЦЕЛЕНИЕ' 
-        desc='упорядоченное отображение взаимного расположения планет на звёздном небе в определенный промежуток времени по знакам зодиака. Используется в астрологии с целью предсказания судьбы.' 
+        desc='набор практик, который научит быть в настоящем моменте и не оставаться в прошлом, не бежать в будущее' 
         color='black' right/>
         <div className={styles.waves3} id='numerology'>
           <img src='/img/backgrounds/waves.svg' alt=''/>
         </div>
         <Content lottie={<Numerology/>} image='/img/Content/numerology.svg' title='НУМЕРОЛОГИЯ' 
-        desc='упорядоченное отображение взаимного расположения планет на звёздном небе в определенный промежуток времени по знакам зодиака. Используется в астрологии с целью предсказания судьбы.' 
+        desc='цифровой расчет чисел, сопровождающих и влияющих на вашу жизнь. Узнайте что говорят о вас цифры' 
         color='blue'/>
         <div className={styles.waves2} id='calendar'>
           <img src='/img/backgrounds/waves2.svg' alt=''/>
         </div>
         <Content lottie={<Calendar/>} image='/img/Content/calendar.svg' title='КАЛЕНДАРЬ КРАСОТЫ' 
-        desc='упорядоченное отображение взаимного расположения планет на звёздном небе в определенный промежуток времени по знакам зодиака. Используется в астрологии с целью предсказания судьбы.' 
+        desc='эффективно планируй процедуры в салоне красоты с помощью этой рубрики. Подсказки на каждый день в зависимости от астрологических трендов' 
         color='black' right/>
         <div className={styles.waves3} id='ball'>
           <img src='/img/backgrounds/waves.svg' alt=''/>
         </div>
         <Content lottie={<MagicBall/>} image='/img/Content/magicBall.svg' title='ШАР ПРЕДСКАЗАНИЙ' 
-        desc='упорядоченное отображение взаимного расположения планет на звёздном небе в определенный промежуток времени по знакам зодиака. Используется в астрологии с целью предсказания судьбы.' 
+        desc='узнай ответ на самый волнующий вопрос. Ваш помощник в сомнительных ситуациях' 
         color='blue'/>
         <div className={styles.waves2} id='tree'>
           <img src='/img/backgrounds/waves2.svg' alt=''/>
         </div>
         <Content lottie={<Tree/>} image='/img/Content/tree.svg' title='ДЕРЕВО ЖИЗНИ' 
-        desc='упорядоченное отображение взаимного расположения планет на звёздном небе в определенный промежуток времени по знакам зодиака. Используется в астрологии с целью предсказания судьбы.' 
+        desc='у каждого человека свое дерево жизни. Оно помогает раскрыть некоторые моменты в судьбе человека. Получите важный совет в жизни' 
         color='black' right/>
         </div>
       <Footer/>
